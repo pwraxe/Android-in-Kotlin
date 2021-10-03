c8ff8dd30bf3d44ed63c14fe78734c2124d69177
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2021-10-03 15:58:54 +0530

    Create 1. Android Notes.kt

---
+++ 1. Android Notes.kt
@@ -0,0 +1,9 @@
+- 1dp = 1/160 inch
+
+- shadow to text => 
+      android:shadowColor="@android:color/holo_blue_dark"
+      android:shadowDx="10"
+      android:shadowDy="0"
+      android:shadowRadius="10"
+      
+      
