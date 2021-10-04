1372c3104f6079299b1c7106c4fc9357742fc0ec
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2021-10-04 08:57:37 +0530

    Update 1. Android Notes.kt

--- 1. Android Notes.kt
+++ 1. Android Notes.kt
@@ -8,5 +8,7 @@
 
 - (1..100).random()
 - All Layouts is type of viewgroup (Layouts like LL, RL, ConstrintLayout...)
+- An "abstract" class is a class that cannot be instantiated because it is not fully implemented
+      Ex.  You use a sketch (abstract class) to create a blueprint (class) from which you build the actual object instance.
       
       
