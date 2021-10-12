d18ac1004c3eaa7be2922b8e23a7bbeae2489802
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2021-10-12 19:13:16 +0530

    Update 1. Android_CodeLab_Notes.kt

--- 1. Android_CodeLab_Notes.kt
+++ 1. Android_CodeLab_Notes.kt
@@ -244,6 +244,14 @@ ________________________________________________________________________________
 - Add following Dependency
     implementation "androidx.work:work-runtime-ktx:$versions.work"
 
+Few WorkManager Classes
+      - Worker : This is where you put the code for the actual work you want to perform in the background. You`ll extend this class and override the doWork() method.
+      - WorkRequest : This represents a request to do some work. You`ll pass in your Worker as part of creating your WorkRequest. 
+                        When making the WorkRequest you can also specify things like Constraints on when the Worker should run.
+
+      - WorkManager: This class actually schedules your WorkRequest and makes it run. 
+                        It schedules WorkRequests in a way that spreads out the load on system resources, while honoring the constraints you specify.
+
 
 
 
