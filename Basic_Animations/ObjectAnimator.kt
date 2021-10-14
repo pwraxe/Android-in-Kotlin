97263e97de689f275ef9d0a5590315367a6580f3
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2021-10-14 12:07:15 +0530

    Create ObjectAnimator.kt

---
+++ Basic_Animations/ObjectAnimator.kt
@@ -0,0 +1,19 @@
+Rotate
+    - val animator = ObjectAnimator.ofFloat(star, View.ROTATION,-360f,0f)
+      animator.duration = 3000L
+      animator.start()
+      
+      //if you want to add listener to animation
+     animator.addListener(object : AnimatorListenerAdapter(){
+
+            override fun onAnimationStart(animation: Animator?) { super.onAnimationStart(animation)  }
+            override fun onAnimationStart(animation: Animator?, isReverse: Boolean) { super.onAnimationStart(animation, isReverse)  }
+            override fun onAnimationResume(animation: Animator?) { super.onAnimationResume(animation)  }
+            override fun onAnimationPause(animation: Animator?) { super.onAnimationPause(animation)  }
+            override fun onAnimationEnd(animation: Animator?) { super.onAnimationEnd(animation)  }
+            override fun onAnimationEnd(animation: Animator?, isReverse: Boolean) { super.onAnimationEnd(animation, isReverse)  }
+            override fun onAnimationRepeat(animation: Animator?) { super.onAnimationRepeat(animation)  }
+            override fun onAnimationCancel(animation: Animator?) { super.onAnimationCancel(animation)  }
+    })
+     
+     
