659b749e4b8ee992327f45d454b1a884370f4235
Akshay S. Pawar <31787030+pwraxe@users.noreply.github.com>
2024-06-02 09:48:47 +0530

    Create MainActivity.kt

---
+++ Jetpack Compose/Compose Quick Guide/PasswordToggle/MainActivity.kt
@@ -0,0 +1,63 @@
+/**
+If you want more icons then add following dependency
+implementation (libs.androidx.material.icons.extended)
+
+**/
+
+package com.example.myapplication
+
+import ...
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                Show()
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun ShowPreview(modifier: Modifier = Modifier) {
+    Show(modifier)
+}
+
+@Composable
+fun Show(modifier: Modifier = Modifier) {
+    var password by remember { mutableStateOf("") }
+    var showPassword by remember { mutableStateOf(false) }
+    val passwordVisualTransformation = remember { PasswordVisualTransformation() }
+
+    TextField(
+        value = password,
+        onValueChange = { password = it },
+        label = {
+            Text(text = "Enter Password")
+        },
+
+        visualTransformation = if (showPassword)
+            VisualTransformation.None
+        else passwordVisualTransformation,
+
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+
+        modifier = modifier.fillMaxWidth(),
+
+        trailingIcon = {
+            Icon(
+                imageVector = if (showPassword)
+                    Icons.Filled.Visibility
+                else Icons.Filled.VisibilityOff,
+
+                contentDescription = null,
+
+                modifier = modifier.clickable {
+                    showPassword = !showPassword
+                }
+            )
+        }
+    )
+}
